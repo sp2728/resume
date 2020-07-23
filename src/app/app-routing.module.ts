28412f348e8ded51be4a99f61a2d9bf07d544088
sp2728 <sp2728@njit.edu>
2020-07-23 16:10:10 -0400

    Added the projects, experience and certications to about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     path:'', redirectTo:'about', pathMatch:'full'
   },
   {
-    path:'contact', component:ContactComponent
+    path:'contact', component:ContactComponent,pathMatch: 'full'
   }
 ];
 
